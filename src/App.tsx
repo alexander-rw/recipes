b51df7842116cfcc2efd0db96e0fa0a4443b738e
outworldsoftware <alexrw17@gmail.com>
2020-07-08 23:56:12 +0100

    kick of recipes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,43 @@
-import React from 'react';
-import { Container } from "@material-ui/core";
+import React, { useEffect, Suspense, useState } from 'react';
+import { Container, AppBar, Typography, Toolbar, makeStyles } from '@material-ui/core';
+import axios from 'axios';
 
-const App: React.FC = () => (
-  <Container maxWidth="sm">
-      <h1>App</h1>
-  </Container>
-);
+import { Recipe } from "./@types/Recipe";
+import { DisplayRecipe } from "./Components/DisplayRecipe";
+
+const useStyles = makeStyles(theme => ({
+  offset: theme.mixins.toolbar,
+}));
+
+const App: React.FC = () => {
+  const classes = useStyles();
+
+  const [data, setRecipes] = useState<{ recipes: Recipe[] }>({ recipes: [] });
+
+  useEffect(() => {
+    async function getRecipes() {
+      return (await axios.get("/recipes.json")).data.recipes as Recipe[];
+    }
+    getRecipes().then(r => { 
+      console.log(`Recipes loaded: ${r.length}`);
+      setRecipes({ recipes: r });
+    });
+  });
+
+  return (
+    <Container maxWidth="sm">
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" color="inherit">
+            Recipes
+          </Typography>
+        </Toolbar>
+      </AppBar>
+        <Suspense fallback={<div>Loading...</div>}>
+          { data.recipes.map((r: Recipe, i: number) => <DisplayRecipe recipe={r} key={i} />)}
+        </Suspense>
+    </Container>
+  );
+}; 
 
 export { App };
--- src/Components/DisplayRecipe.tsx
+++ src/Components/DisplayRecipe.tsx
@@ -1,12 +1,35 @@
 import React from "react";
 
 import { Recipe } from "../@types/Recipe";
-import { Accordion } from '@material-ui/core';
-export const DisplayRecipe: React.FC<Recipe> = ({ name, originalSource }) => (
-  <Accordion>
-    {name}
-    {" "}
-    {originalSource}
-  </Accordion>
-);
+import { Accordion, AccordionSummary, AccordionDetails, Typography, makeStyles } from '@material-ui/core';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
+  },
+}));
+
+export const DisplayRecipe: React.FC<{ recipe: Recipe; key: number }> = ({ recipe }) => {
+  const classes = useStyles();
+
+  return (
+    <Accordion>
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon />}
+        aria-controls="panel1a-content"
+        id="panel1a-header"
+      >
+        <Typography className={classes.heading}>{recipe.name}</Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+          <Typography>{recipe.originalSource}</Typography>
+      </AccordionDetails>  
+    </Accordion>
+  );
+};
 
